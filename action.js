0d3a5b81c5adad667e85134c357892ba9dcb3cb6
ghyda Alhajajrah <ghaidahjajrah02@gmail.com>
2024-01-31 10:49:55 +0300

    g

--- action.js
+++ action.js
@@ -1,1 +1,23 @@
+function handleMouseOver(element) {
+   
+    element.style.border = '2px solid #333'; 
+}
 
+function handleMouseOut(element) {
+    
+    element.style.border = 'none'; 
+}
+
+
+
+//   // define a function to change the pictures
+//   function turnOn() {
+//     document.getElementById("clod").src = "image\product_1.png";
+//     document.getElementById("clod1").src = "image\product_2.png";
+//   }
+  
+//   // define a function to change the pictures back
+//   function turnOff() {
+//     document.getElementById("pic1").src = "/images/adc9dffcaf0582934c7566998f9f5bbd.jpg";
+//     document.getElementById("pic2").src = "/images/36aa7302ed45b2252f67889fdafa377b.jpg";
+//   }
